521201dc093e40905d6082a18a2c700799fd02c9
Joshua <tantohyung@gmail.com>
2021-07-20 08:54:04 +0800

    enable schedule scaling, multitags,  ami config

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -33,8 +33,8 @@ data "template_file" "user_data" {
     server_port = var.server_port
     db_address = data.terraform_remote_state.db.outputs.address
     db_port = data.terraform_remote_state.db.outputs.port
+    server_text = var.server_text
   }
-
 }
 
 resource "aws_lb" "example" {
@@ -79,10 +79,11 @@ resource "aws_lb_target_group" "asg" {
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id = "ami-0c55b159cbfafe1f0"
+  image_id = var.ami
   instance_type = var.instance_type
   security_groups = [aws_security_group.instance.id]
   user_data = data.template_file.user_data.rendered
+
   # Required when using a launch configuration with an auto scaling group.
   # Create replacement before destroy
   lifecycle {
@@ -91,6 +92,10 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
+  # Explicitly depend on the launch configuration name so that each time it is changed,
+  # the ASG is forced to change
+  name = "${var.cluster_name}-${aws_launch_configuration.example.name}"
+
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
   target_group_arns = [aws_lb_target_group.asg.arn]
@@ -99,11 +104,54 @@ resource "aws_autoscaling_group" "example" {
   min_size = var.min_size
   max_size = var.max_size
 
+  # Wait for at least this many instances to pass health checks before considering ASG deployment complete
+  min_elb_capacity = var.min_size
+
   tag {
     key = "Name"
     value = "${var.cluster_name}-asg-example"
     propagate_at_launch = true
   }
+
+  dynamic "tag" {
+    for_each = {
+      for key, value in var.custom_tags:
+        key => upper(value)
+        if key != "Name"
+    }
+    content {
+      key = tag.key
+      value = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  # When replacing this ASG, create replacement first before destroying
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_biz_hrs" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "${var.cluster_name}-scale-out-during-biz-hrs"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "${var.cluster_name}-scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+
+  autoscaling_group_name = aws_autoscaling_group.example.name
 }
 
 resource "aws_lb_listener_rule" "asg" {
@@ -167,4 +215,40 @@ resource "aws_security_group_rule" "alb_allow_all_outbound" {
   to_port = local.any_port
   protocol = local.any_protocol
   cidr_blocks = local.all_ips  
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cp" {
+  alarm_name = "${var.cluster_name}-high-cpu-utilization"
+  namespace = "AWS/EC2"
+  metric_name = "CPUUtilization"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.example.name
+  }
+
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods = 1
+  period = 300
+  statistic = "Average"
+  threshold = 90
+  unit = "Percent"
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
+  count = format("%.1s", var.instance_type) == "t" ? 1 : 0
+
+  alarm_name = "${var.cluster_name}-low-cpu-credit-balance"
+  namespace = "AWS/EC2"
+  metric_name = "CPUCreditBalance"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.example.name
+  }
+
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods = 1
+  period = 300
+  statistic = "Minimum"
+  threshold = 10
+  unit = "Count"
 }
--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -33,3 +33,26 @@ variable "max_size" {
   description = "The maximum number of EC2 Instances in the ASG"
   type = number
 }
+
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type = map(string)
+  default = {}
+}
+
+variable "enable_autoscaling" {
+  description = "If set to true, enable auto scaling"
+  type = bool
+}
+
+variable "ami" {
+  description = "The AMI to run in the cluster"
+  default = "ami-0c55b159cbfafe1f0"
+  type = string
+}
+
+variable "server_text" {
+  description = "The text that the web should return"
+  default = "Hello World"
+  type = string
+}
