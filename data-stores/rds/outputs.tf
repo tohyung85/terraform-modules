dd0ee4c466c9c3096346d432698b6a7cad45a9f0
Joshua <tantohyung@gmail.com>
2021-09-06 18:37:35 +0800

    add db name

--- data-stores/rds/outputs.tf
+++ data-stores/rds/outputs.tf
@@ -8,6 +8,11 @@ output "port" {
   description = "The port the database is listening on"
 }
 
+output "name" {
+  value       = length(var.subnet_ids) > 0 ? aws_db_instance.db_custom_subnet[0].name : aws_db_instance.db_default_subnet[0].name
+  description = "Name of terraform rds"
+}
+
 output "username" {
   value       = var.db_username
   description = "DB User name"
